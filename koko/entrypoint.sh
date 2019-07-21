b2652ac2f625237971ff884a57cf9017ce3ba0e0
wojiushixiaobai <296015668@qq.com>
2019-07-21 15:33:53 +0800

    [Fix]修改koko启动脚本

--- koko/entrypoint.sh
+++ koko/entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 #
 
-if [ ! -f "/opt/coco/config.yml" ]; then
-    cp /opt/coco/config_example.yml /opt/coco/config.yml
-    sed -i '5d' /opt/coco/config.yml
-    sed -i "5i CORE_HOST: $CORE_HOST" /opt/coco/config.yml
-    sed -i "s/BOOTSTRAP_TOKEN: <PleasgeChangeSameWithJumpserver>/BOOTSTRAP_TOKEN: $BOOTSTRAP_TOKEN/g" /opt/coco/config.yml
-    sed -i "s/# LOG_LEVEL: INFO/LOG_LEVEL: ERROR/g" /opt/coco/config.yml
-fi
-
 sleep 5s
 while ! nc -z core 8080;
 do
@@ -16,6 +8,14 @@ do
     sleep 2s
 done
 sleep 5s
+
+if [ ! -f "/opt/coco/config.yml" ]; then
+    cp /opt/coco/config_example.yml /opt/coco/config.yml
+    sed -i '5d' /opt/coco/config.yml
+    sed -i "5i CORE_HOST: $CORE_HOST" /opt/coco/config.yml
+    sed -i "s/BOOTSTRAP_TOKEN: <PleasgeChangeSameWithJumpserver>/BOOTSTRAP_TOKEN: $BOOTSTRAP_TOKEN/g" /opt/coco/config.yml
+    sed -i "s/# LOG_LEVEL: INFO/LOG_LEVEL: ERROR/g" /opt/coco/config.yml
+fi
 
 cd /opt/coco
 ./koko
