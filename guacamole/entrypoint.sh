500d500fcc89cd84ead3fe59843de22a8ba685ab
Administrator <admin@example.com>
2019-10-10 09:47:44 +0800

    [Fix]修复koko版本

--- guacamole/entrypoint.sh
+++ guacamole/entrypoint.sh
@@ -14,7 +14,7 @@ done
 guacd &
 cd /config/tomcat9/bin && ./startup.sh
 
-echo "Guacamole version 1.5.2, more see https://www.jumpserver.org"
+echo "Guacamole version 1.5.3, more see https://www.jumpserver.org"
 echo "Quit the server with CONTROL-C."
 
 if [ ! -f "/config/guacamole/data/log/info.log" ]; then
