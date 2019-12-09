6e025271c2262f186b60378b0270a9b2d480df9c
wojiushixiaobai <296015668@qq.com>
2019-12-09 16:57:54 +0800

    [Update]更改upload目录

--- koko/entrypoint.sh
+++ koko/entrypoint.sh
@@ -14,6 +14,7 @@ if [ ! -f "/opt/koko/config.yml" ]; then
     sed -i "5i CORE_HOST: $CORE_HOST" /opt/koko/config.yml
     sed -i "s/BOOTSTRAP_TOKEN: <PleasgeChangeSameWithJumpserver>/BOOTSTRAP_TOKEN: $BOOTSTRAP_TOKEN/g" /opt/koko/config.yml
     sed -i "s/# LOG_LEVEL: INFO/LOG_LEVEL: ERROR/g" /opt/koko/config.yml
+    sed -i "s@# SFTP_ROOT: /tmp@SFTP_ROOT: /@g" /opt/koko/config.yml
 fi
 
 cd /opt/koko
